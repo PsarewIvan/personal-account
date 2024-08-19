f5750678029192aba89a6593bfc88740bd262cd9
Ivan Psarew <psarew.ivan@yandex.ru>
2024-08-19 21:28:07 +0600

    fix: select

--- docs/js/index.js
+++ docs/js/index.js
@@ -194,18 +194,24 @@
 })();
 
 (() => {
-    const selects = document.querySelectorAll('.js-select-component');
+    const selectsWrapper = document.querySelectorAll(
+        '.js-select-component-wrapper'
+    );
 
-    selects.forEach((select) => {
-        const placeholder = select.dataset.placeholder;
-        const options = select.querySelectorAll('option');
+    console.log(selectsWrapper);
+
+    selectsWrapper.forEach((selectWrapper) => {
+        const select = selectWrapper.querySelector('.js-select-component');
+        const placeholder = select?.dataset.placeholder;
+        const options = select?.querySelectorAll('option');
+        const reset = selectWrapper?.querySelector('.js-select-reset');
 
         const dataOptions = Array.from(options).map((option) => ({
             text: option.innerHTML,
             value: option.value,
         }));
 
-        new SlimSelect({
+        const slimSelect = new SlimSelect({
             select,
             settings: {
                 showSearch: false,
@@ -216,6 +222,37 @@
                     : []),
                 ...dataOptions,
             ],
+            events: {
+                afterChange: (evt) => {
+                    const value = evt?.[0]?.value;
+
+                    if (value) {
+                        reset.classList.remove('hidden');
+                    }
+                },
+            },
+        });
+
+        reset?.addEventListener('click', () => {
+            if (slimSelect) {
+                slimSelect.setSelected('', false);
+                reset.classList.add('hidden');
+            }
+        });
+
+        const selectDiv = selectWrapper.querySelector(
+            `div[data-id="${slimSelect?.settings?.id}"]`
+        );
+        const arrow = selectWrapper.querySelector('.ss-arrow');
+
+        select?.addEventListener('change', (event) => {
+            if (dataOptions.some(({ value }) => value === event.target.value)) {
+                selectDiv.classList.add('select-component_selected');
+                arrow?.classList.add('transparent');
+            } else {
+                selectDiv.classList.remove('select-component_selected');
+                arrow?.classList.remove('transparent');
+            }
         });
     });
 })();
